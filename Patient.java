efbe161fe3d001d04e97c3d3e26ee2784e7c1421
Jared Moore <CtjaredM2@gmail.com>
2022-04-21 17:27:59 -0400

    Changed Patient.java

--- Patient.java
+++ Patient.java
@@ -4,15 +4,30 @@ public class Patient {
     private enum PatientType{PEDIATRIC, ADULT, GERIATRIC}
     private float coPay;
 
-    public Patient(String tLastName, String tFirstName, String tAddress, String tPhoneNum, String tDoB, String tInsuranceType, float tCoPay, String tPatientType){
+    // Constructor
+    public Patient(String tLastName, String tFirstName, String tAddress, String tPhoneNum, String tDoB, String tInsuranceType, float tCoPay, String tPatientType, String tphysName, String tphysNumber, String tallergies, String tillnesses){
         lastName = tLastName;
         firstName = tFirstName;
         address = tAddress;
+        phoneNum = tPhoneNum;
+        DoB = tDoB;
+        insuranceType insType;
+        if (tInsuranceType == "Private"){
+            insType = insuranceType.PRIVATE;
+        } else if (tInsuranceType == "Government") {
+            insType = insuranceType.GOVERNMENT;
+        }
+
+        coPay = tCoPay;
+        PatientType = tPatientType;
 
     }
+    // Gets
+    // Updates
 
     class MedicalConditions{
         private String physName, physNumber;
         private enum allergies{FOOD, MEDICATION, SEASONAL, NONE, OTHER}
-    }   private enum illnesses{DIABETES, CHD, ASTHMA, NONE, OTHER}
+        private enum illnesses{DIABETES, CHD, ASTHMA, NONE, OTHER}
+        }
 }

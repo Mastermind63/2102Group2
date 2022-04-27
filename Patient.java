f571667ccb9f3b540fb904041ddf590ae4de3b73
Jared Moore <CtjaredM2@gmail.com>
2022-04-27 00:01:01 -0400

    Changed print to return in PrintSummary

--- Patient.java
+++ Patient.java
@@ -228,8 +228,10 @@ public class Patient {
     }
 
     // Print only name & dob
-    public void PrintSummary() {
+    public String PrintSummary() {
         System.out.println("Patient Name: " + getLastName() + ", " + getFirstName());
         System.out.println("Patient DOB: " + getDoB() + "\n");
+        return("\"Patient Name: \" + getLastName() + \", \" + getFirstName()\n"+
+                "Patient DOB: " + getDoB() + "\n");
     }
 }
